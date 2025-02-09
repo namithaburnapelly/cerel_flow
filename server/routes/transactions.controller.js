03614f0b4f51538af565e3e649eba2e189ff8749
namithaburnapelly <namitha.nja@gmail.com>
2025-02-09 15:40:13 +0530

    transactions server setup

--- server/routes/transactions.controller.js
+++ server/routes/transactions.controller.js
@@ -8,12 +8,11 @@ router.get("/:userId", async (req, res) => {
   try {
     const db = getDb();
 
-    const user = await db
-      .collection("transactions")
-      .findOne(
-        { userId: req.params.userId },
-        { projection: { _id: 0, userId: 1, transactions: 1 } }
-      );
+    const user = await db.collection("transactions").findOne(
+      { userId: req.params.userId },
+      //projection displays these values in the result output where; 0 - dont view, 1 - view
+      { projection: { _id: 0, userId: 1, transactions: 1 } }
+    );
 
     //check if user has transactions
     if (!user || user.transactions.length === 0) {
@@ -67,17 +66,58 @@ router.post("/:userId", async (req, res) => {
   }
 });
 
+//to update a existing transaction
+router.patch("/:userId/:transactionId", async (req, res) => {
+  try {
+    const { userId, transactionId } = req.params;
+    const updatedData = req.body; //contains fields that need to be updated
+
+    if (Object.keys(updatedData).length === 0) {
+      return res
+        .status(400)
+        .json({ message: "No fields provided for update." });
+    }
+
+    const db = getDb();
+
+    //map user input keys to mongodb update format
+    const updatedFileds = Object.fromEntries(
+      Object.entries(updatedData).map(([key, value]) => [
+        `transactions.$[element].${key}`, //map keys correctly to mongodb update
+        value,
+      ])
+    );
+
+    //updating the fields
+    const result = await db.collection("transactions").updateOne(
+      { userId: userId, "transactions.transactionId": transactionId }, //finds the correct userid and  transaction id
+      { $set: updatedFileds }, //set applies the maped updated
+      { arrayFilters: [{ "element.transactionId": transactionId }] } //filter the correct transaction in array
+    );
+
+    if (result.modifiedCount > 0) {
+      return res
+        .status(200)
+        .json({ message: "Transaction updated successfully." });
+    } else {
+      return res.status(404).json({ message: "Transaction not found." });
+    }
+  } catch (error) {
+    console.log(error);
+    //serve error
+    res.status(500).json({ message: "Server error." });
+  }
+});
+
 router.delete("/:userId/:transactionId", async (req, res) => {
   try {
     const { userId, transactionId } = req.params;
 
     const db = getDb();
-    const result = await db
-      .collection("transactions")
-      .updateOne(
-        { userId: userId },
-        { $pull: { transactions: { transactionId: transactionId } } }
-      );
+    const result = await db.collection("transactions").updateOne(
+      { userId: userId }, //finds the user id in which the aciton to be done
+      { $pull: { transactions: { transactionId: transactionId } } } //pull removes the transaction where transaction maps
+    );
 
     if (result.modifiedCount > 0) {
       return res
