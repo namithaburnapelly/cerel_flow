aa64bb05f090d50ed416f5b8650ee03741e58c71
namithaburnapelly <namitha.nja@gmail.com>
2025-03-08 03:09:57 +0530

    feature module implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Authentication/login/login.component';
 import { AuthService } from './Authentication/Service/auth.service';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { HomeComponent } from './expense/Components/Layout/home/home.component';
 import { RegisterComponent } from './Authentication/register/register.component';
 import { authFactory } from './Authentication/utils/user_auth';
 import { LoginResolve } from './Authentication/Service/login-resolve.service';
@@ -20,64 +19,30 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { JWT_Module_Options } from './Authentication/utils/jwt_auth';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrorHandlerService } from './expense/utils/error-handler';
-import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { TransactionService } from './expense/Service/Transaction/transaction.service';
 import { authInterceptor } from './Authentication/Service/auth.interceptor';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { transferReducer } from './expense/@NgRx/Transfers/transfer.reducers';
 import { TransferService } from './expense/Service/Transfer/transfer.service';
-import { TransferEffects } from './expense/@NgRx/Transfers/transfer.effects';
-import { RecentPaymentComponent } from './expense/Components/Payments/recent-payment/recent-payment.component';
-import { NewTransferComponent } from './expense/Components/Payments/new-transfer/new-transfer.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { UpdateTransferComponent } from './expense/Components/Payments/@Angular_material/update-transfer/update-transfer.component';
-import { TransferformComponent } from './expense/Components/Transactions/Forms/transferform/transferform.component';
-import { TransactionFormComponent } from './expense/Components/Transactions/Forms/transaction-form/transaction-form.component';
-import { TransactionListComponent } from './expense/Components/Transactions/Lists/transaction-list/transaction-list.component';
-import { TransferListComponent } from './expense/Components/Transactions/Lists/transfer-list/transfer-list.component';
-import { ConfirmDeleteDialogComponent } from './expense/Components/Payments/@Angular_material/confirm-delete-dialog/confirm-delete-dialog.component';
-import { UpdateTransactionComponent } from './expense/Components/Payments/@Angular_material/update-transaction/update-transaction.component';
-import { NavbarComponent } from './expense/Components/Layout/navbar/navbar.component';
-import { transactionReducer } from './expense/@NgRx/Transactions/transaction.reducers';
-import { TransactionEffects } from './expense/@NgRx/Transactions/transaction.effects';
-import { NewTransactionComponent } from './expense/Components/Payments/new-transaction/new-transaction.component';
+import { ExpenseModule } from './expense/expense.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    TransactionListComponent,
-    TransactionFormComponent,
     RegisterComponent,
-    NavbarComponent,
     PagenotfoundComponent,
-    TransferformComponent,
-    RecentPaymentComponent,
-    NewTransferComponent,
-    TransferListComponent,
-    UpdateTransferComponent,
-    ConfirmDeleteDialogComponent,
-    UpdateTransactionComponent,
-    NewTransactionComponent,
   ],
   imports: [
     BrowserModule,
-    NgxPaginationModule, //pagination module from npm
-    ReactiveFormsModule,
-    RouterModule,
     AppRoutingModule,
-    FormsModule,
-    MatDialogModule,
     JwtModule.forRoot(JWT_Module_Options),
-    StoreModule.forRoot({
-      transactions: transactionReducer,
-      transfers: transferReducer,
-    }),
-    EffectsModule.forRoot([TransactionEffects, TransferEffects]), //to register effects
+    ReactiveFormsModule,
+    FormsModule,
+    EffectsModule.forRoot([]), // Make sure EffectsModule is initialized
+    StoreModule.forRoot({}), //This initializes NgRx Store globally
+    ExpenseModule,
   ],
   providers: [
     //provide custom error handler class to app module.
--- src/app/expense/expense.module.ts
+++ src/app/expense/expense.module.ts
@@ -2,13 +2,56 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExpenseRoutingModule } from './expense-routing.module';
-
+import { HomeComponent } from './Components/Layout/home/home.component';
+import { NavbarComponent } from './Components/Layout/navbar/navbar.component';
+import { RecentPaymentComponent } from './Components/Payments/recent-payment/recent-payment.component';
+import { NewTransactionComponent } from './Components/Payments/new-transaction/new-transaction.component';
+import { TransactionListComponent } from './Components/Transactions/Lists/transaction-list/transaction-list.component';
+import { TransactionFormComponent } from './Components/Transactions/Forms/transaction-form/transaction-form.component';
+import { TransferformComponent } from './Components/Transactions/Forms/transferform/transferform.component';
+import { NewTransferComponent } from './Components/Payments/new-transfer/new-transfer.component';
+import { TransferListComponent } from './Components/Transactions/Lists/transfer-list/transfer-list.component';
+import { UpdateTransferComponent } from './Components/Payments/@Angular_material/update-transfer/update-transfer.component';
+import { ConfirmDeleteDialogComponent } from './Components/Payments/@Angular_material/confirm-delete-dialog/confirm-delete-dialog.component';
+import { UpdateTransactionComponent } from './Components/Payments/@Angular_material/update-transaction/update-transaction.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { RouterModule } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
+import { StoreModule } from '@ngrx/store';
+import { transactionReducer } from './@NgRx/Transactions/transaction.reducers';
+import { transferReducer } from './@NgRx/Transfers/transfer.reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { TransactionEffects } from './@NgRx/Transactions/transaction.effects';
+import { TransferEffects } from './@NgRx/Transfers/transfer.effects';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    TransactionListComponent,
+    TransactionFormComponent,
+    NavbarComponent,
+    TransferformComponent,
+    RecentPaymentComponent,
+    NewTransferComponent,
+    TransferListComponent,
+    UpdateTransferComponent,
+    ConfirmDeleteDialogComponent,
+    UpdateTransactionComponent,
+    NewTransactionComponent,
+  ],
   imports: [
     CommonModule,
-    ExpenseRoutingModule
-  ]
+    ExpenseRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule, //pagination module from npm
+    RouterModule,
+    MatDialogModule,
+    StoreModule.forFeature('transactions', transactionReducer),
+    StoreModule.forFeature('transfers', transferReducer),
+    EffectsModule.forFeature([TransactionEffects, TransferEffects]), //to register effects
+  ],
+  exports: [HomeComponent, NavbarComponent],
 })
-export class ExpenseModule { }
+export class ExpenseModule {}
