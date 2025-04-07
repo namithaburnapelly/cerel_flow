5c5b421ffad3d556bbdc69eb0c2b494e565f53a2
namithaburnapelly <namitha.nja@gmail.com>
2025-04-07 17:12:17 +0530

    server updated

--- src/environment/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  loginUrl: 'http://localhost:3000/auth/login',
-  registerUrl: 'http://localhost:3000/auth/register',
-  TRANSACTION_FEATURE_KEY: 'transactions',
-};
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: false,
-  loginUrl: 'http://localhost:3000/auth/login',
-  registerUrl: 'http://localhost:3000/auth/register',
+  loginUrl: 'https://cerel-flow.onrender.com/auth/login',
+  registerUrl: 'https://cerel-flow.onrender.com/auth/register',
 };
 
 export const environmentVariables = {
   transferPageSize: 10,
-  transferUrl: 'http://localhost:3000/transfers',
+  transferUrl: 'https://cerel-flow.onrender.com/transfers',
   TRANSFER_FEATURE_KEY: 'transfers',
 
-  transactionUrl: 'http://localhost:3000/transactions',
+  transactionUrl: 'https://cerel-flow.onrender.com/transactions',
   transactionPageSize: 10,
   TRANSACTION_FEATURE_KEY: 'transactions',
 };
